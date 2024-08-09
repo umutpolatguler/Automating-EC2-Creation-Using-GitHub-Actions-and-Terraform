1578b72851ddf4462160d904888415f83d7c24ae
Umut Polat Güler <61421185+umutpolatguler@users.noreply.github.com>
2024-08-09 13:09:50 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
 	region = var.aws_region
 }
 
-resource "aws_instance" "exapmle" {
+resource "aws_instance" "example" {
 	ami			  = var.ami_id
 	instance_type = var.instance_type
 	
